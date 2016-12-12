94539b2c11c6eca4b5bc46cffabe77a26ee1c08e
Bryan Deeney <bdeeney@pobox.com>
2016-12-12 13:10:40 -0500

    Bump version: 0.2.0 → 0.2.1

--- emanate/__init__.py
+++ emanate/__init__.py
@@ -3,4 +3,4 @@ from emanate.events import Event
 
 __author__ = 'YouGov, plc'
 __email__ = 'dev@yougov.com'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requirements = [
 
 setup(
     name='emanate',
-    version='0.2.0',
+    version='0.2.1',
     description="Lightweight event system for Python",
     long_description=readme + '\n\n' + history,
     author="YouGov, plc",
