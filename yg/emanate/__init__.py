9737eced8e2d667e3413a7d65946658d94f5868c
Jason R. Coombs <jason.coombs@yougov.com>
2018-06-12 08:16:21 -0400

    Load the version from the package metadata rather than trying to maintain it in a third place.

--- yg/emanate/__init__.py
+++ yg/emanate/__init__.py
@@ -3,6 +3,12 @@ from .events import Event
 
 __author__ = 'YouGov, plc'
 __email__ = 'dev@yougov.com'
-__version__ = '0.3.0'
 
 __all__ = ['Event']
+
+try:
+    import pkg_resources
+    dist = pkg_resources.get_distribution('yg.emanate')
+    __version__ = dist.version
+except Exception:
+    __version__ = 'unknown'
